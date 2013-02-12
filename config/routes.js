768d248592104221049f97c938a9c0aa95645f5a
Wolfgang Merkt <w.merkt@gmail.com>
2013-02-11 18:51:48 -0600

    renamed test index files, changed routes

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,10 @@
 exports.routes = function (map) {
+    map.root('posts#index');
+    map.get('/', 'posts#index');
     map.resources('posts');
 
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
     map.all(':controller/:action/:id');
-    
-    map.root('posts#index');
 };
